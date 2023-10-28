622fed0ed07f8ac78f5077cab0db28d127f3ded5
Aditya <aditya@Adityas-MacBook-Air.local>
2023-10-28 16:01:58 +0530

    Oppenheimer was envious

--- coms/fibonacci.php
+++ coms/fibonacci.php
@@ -1,3 +1,19 @@
 <?php
-echo "Hello, World!";
+function generateFibonacci($n) {
+    $fibonacci = [0, 1]; // Initialize the first two Fibonacci numbers
+
+    for ($i = 2; $i < $n; $i++) {
+        $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
+    }
+
+    return $fibonacci;
+}
+
+$n = 10; // Number of Fibonacci numbers to generate
+$fibonacciSequence = generateFibonacci($n);
+
+echo "Fibonacci Sequence of $n numbers: ";
+foreach ($fibonacciSequence as $number) {
+    echo $number . " ";
+}
 ?>
