515af5472834cbb5f3de9f1d8fcf3994e455327f
Aditya <aditya@Adityas-MacBook-Air.local>
2023-11-02 12:34:18 +0530

    Factorial was added

---
+++ coms/factorial.php
@@ -0,0 +1,13 @@
+<?php
+function factorial($n) {
+    if ($n === 0) {
+        return 1;
+    } else {
+        return $n * factorial($n - 1);
+    }
+}
+
+$number = 5;
+$result = factorial($number);
+echo "The factorial of $number is $result";
+?>
